eeb605299af6e4df14e29486f62d88d42ad00442
Santanu Bera <santanu.bera@computronicsonline.com>
2023-10-10 16:03:26 +0530

    x

---
+++ projects/labani/web/src/routes/apps/bookmark/Header.svelte
@@ -0,0 +1,41 @@
+<header>
+	<div class="website_title">Bookmark Manager</div>
+	<div class="menu_items">
+		<Button title="Create Bookmark" on:tap={buttonTap}></Button>
+	</div>
+</header>
+<script>
+import Button from 'components/SvelteButton.svelte';
+function buttonTap(event){
+	console.log(event);
+}
+</script>
+<style>
+.website_title{
+	font-size: 25px;
+	font-weight: bold;
+	color: #fff;
+}
+header{
+	display: flex;
+	justify-content: flex-start;
+	background: #C33764;  /* fallback for old browsers */
+	background: -webkit-linear-gradient(to right, #1D2671, #C33764);  /* Chrome 10-25, Safari 5.1-6 */
+	background: linear-gradient(to right, #1D2671, #C33764); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+	align-items: center;
+	height: 60px;
+	position: fixed;
+	left: 0;
+	right: 0;
+	top: 0;
+	z-index: 10001;
+    padding-left: 20px;
+}
+.menu_items{
+    display: flex;
+    flex: 1;
+    justify-content: flex-end;
+    align-items: center;
+	padding-right: 20px;
+}
+</style>
