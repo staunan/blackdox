8025d644facb7936d91f31942fe7e6b5a249830f
Santanu Bera <santanu.bera@computronicsonline.com>
2025-02-27 08:27:27 +0530

    x

---
+++ projects/routinely/web/src/components/tabs/DaisyTab.svelte
@@ -0,0 +1,51 @@
+<script>
+	let tabs = [
+		{
+			id: 1,
+			label: "Daily",
+			id: 2,
+			label: "Weekly",
+			id: 3,
+			label: "Monthly",
+			id: 4,
+			label: "Yearly",
+		},
+	];
+	let selectedTab = tabs[0];
+	function tabClickedHandler(event) {
+		selectedTab = tab;
+	}
+</script>
+
+<div class="tabs">
+	{#each tabs as tab}
+		<!-- svelte-ignore a11y_click_events_have_key_events -->
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		<div
+			class="tab_item"
+			class:selected={selectedTab.id == tab.id}
+			on:click={() => tabClickedHandler(tab)}
+			title={tab.label}
+		>
+			{tab.label}
+		</div>
+	{/each}
+</div>
+<div class="tab_content_one"></div>
+<div class="tab_content_two"></div>
+<div class="tab_content_three"></div>
+<div class="tab_content_four"></div>
+
+<style>
+	.tabs {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		font-family: monospace;
+	}
+	.tab_item {
+		padding-right: 30px;
+		font-size: 16px;
+		padding-top: 3;
+	}
+</style>
---
+++ projects/routinely/web/src/routes/routines/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import CarbonTab from "components/tabs/CarbonTab.svelte";
+</script>
+
+<svelte:head>
+	<title>Routines</title>
+	<meta
+		name="List of routines. Explore your routines by daily, weely, monthly or yearly, create groups like 'Morning Routines', 'Evening Routine' in Daily tabs, create group like 'Christmas Routine' in Yearly tab. Check documentation for more info."
+		content="List of routines"
+	/>
+</svelte:head>
+
+<div class="page">
+	<CarbonTab></CarbonTab>
+</div>
+
+<style>
+	.page {
+		width: 100%;
+	}
+</style>
